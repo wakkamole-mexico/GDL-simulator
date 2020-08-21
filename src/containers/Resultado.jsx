296163beec339db42f796c699f21767ef399f3d9
CarlosRuiz <ingecarlosrm@gmail.com>
2020-08-21 16:30:15 -0500

    [ADD] result & notfoundpage

--- src/containers/Resultado.jsx
+++ src/containers/Resultado.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import '../assets/styles/components/Resultado.scss';
 
 const Resultado = () => (
-  <p>Resultado</p>
+  <div className='result-container'>
+    <div className='result-container-title'>
+      <h1>
+        Así puede
+        <br />
+        verse tu
+        <br />
+        próximo
+        <br />
+        proyecto
+      </h1>
+    </div>
+    <div className='result-container-video'>
+      <iframe width='560' height='315' src='https://www.youtube.com/embed/BPoQuaTyt-U' frameBorder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowFullScreen />
+    </div>
+
+  </div>
 );
 
 export default Resultado;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Resultado from '../containers/Resultado';
+import NotFound from '../containers/NotFound';
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path='/' component={Home} />
-    <Route exact path='/resultado' component={Resultado} />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/resultado' component={Resultado} />
+      <Route component={NotFound} />
+    </Switch>
   </BrowserRouter>
+
 );
 
 export default App;
