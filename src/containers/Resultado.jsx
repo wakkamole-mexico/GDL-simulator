689a966ee9e334f312e2dcee995167ea441c1a76
CarlosRuiz <ingecarlosrm@gmail.com>
2020-08-24 13:06:11 -0500

    [UPDATE] Css Results

--- src/containers/Resultado.jsx
+++ src/containers/Resultado.jsx
@@ -4,21 +4,31 @@ import '../assets/styles/components/Resultado.scss';
 
 const Resultado = () => (
   <div className='result-container'>
-    <div className='result-container-title'>
-      <h1>
-        Así puede
-        <br />
-        verse tu
-        <br />
-        próximo
-        <br />
-        proyecto
-      </h1>
+    <div className='result-container-main'>
+      <div className='result-container-title'>
+        <h1>
+          Así puede
+          <br />
+          verse tu
+          <br />
+          próximo
+          <br />
+          proyecto
+        </h1>
+      </div>
+      <div className='result-container-video'>
+        <ResponsivePlayer
+          url='https://www.youtube.com/watch?v=BPoQuaTyt-U&t=9s'
+        />
+      </div>
+
     </div>
-    <div className='result-container-video'>
-      <ResponsivePlayer
-      url="https://www.youtube.com/watch?v=BPoQuaTyt-U&t=9s"
-      />
+    <div className='scroll-container'>
+      <p className='scroll-title'>scroll</p>
+      <div className='c-scrolldown'>
+
+        <div className='c-line' />
+      </div>
     </div>
 
   </div>
