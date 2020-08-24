6caba2b755530571a275246971a4a73e382b04f0
CarlosRuiz <ingecarlosrm@gmail.com>
2020-08-24 16:01:55 -0500

    [UPDATE] Salas file styles & animations

---
+++ src/components/Salas.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/Salas.scss';
+
+const Salas = () => (
+  <section className='Salas-container'>
+
+    <h2>
+      Conoce las salas que usaremos
+    </h2>
+  </section>
+);
+
+export default Salas;
--- src/containers/Resultado.jsx
+++ src/containers/Resultado.jsx
@@ -1,36 +1,41 @@
 import React from 'react';
 import ResponsivePlayer from '../components/ResponsivePlayer';
+import Salas from '../components/Salas';
 import '../assets/styles/components/Resultado.scss';
+import '../assets/styles/Animations.scss';
 
 const Resultado = () => (
-  <div className='result-container'>
-    <div className='result-container-main'>
-      <div className='result-container-title'>
-        <h1>
-          Así puede
-          <br />
-          verse tu
-          <br />
-          próximo
-          <br />
-          proyecto
-        </h1>
+  <div>
+    <div className='result-container'>
+      <div className='result-container-main'>
+        <div className='result-container-title fadeIn'>
+          <h1>
+            Así puede
+            <br />
+            verse tu
+            <br />
+            próximo
+            <br />
+            proyecto
+          </h1>
+        </div>
+        <div className='result-container-video'>
+          <ResponsivePlayer
+            url='https://www.youtube.com/watch?v=BPoQuaTyt-U&t=9s'
+          />
+        </div>
+
       </div>
-      <div className='result-container-video'>
-        <ResponsivePlayer
-          url='https://www.youtube.com/watch?v=BPoQuaTyt-U&t=9s'
-        />
+      <div className='scroll-container fadeInUp'>
+        <p className='scroll-title'>scroll</p>
+        <div className='c-scrolldown'>
+
+          <div className='c-line' />
+        </div>
       </div>
 
     </div>
-    <div className='scroll-container'>
-      <p className='scroll-title'>scroll</p>
-      <div className='c-scrolldown'>
-
-        <div className='c-line' />
-      </div>
-    </div>
-
+    <Salas />
   </div>
 );
 
