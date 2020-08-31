8be857f24d3b37c56ebd45068035c1af7c5956ad
CarlosRuiz <ingecarlosrm@gmail.com>
2020-08-30 20:28:59 -0500

    [UPDATE] home styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,19 @@ import '../assets/styles/components/Header.scss';
 
 const Header = () => (
   <header>
-    <p>header</p>
+    <div className='hero--container'>
+      <div className='header--container'>
+        <img src='https://firebasestorage.googleapis.com/v0/b/wakkamole-61f7d.appspot.com/o/gdl-assets%2Flogo.svg?alt=media&token=91aab9b4-5181-4f17-9419-3b26e95a0461' alt='' />
+      </div>
+      <div className="main--container">
+      <div className='title--container'>
+        <img className='title--container__img' src='https://firebasestorage.googleapis.com/v0/b/wakkamole-61f7d.appspot.com/o/gdl-assets%2Fmain-title.svg?alt=media&token=2b2960e8-a681-403c-833a-dba7b0231e73' alt='' />
+      </div>
+      <div className='subtitle--container'>
+        <p>Somos narradores de historias de principio a fin, utilizando cualquier enfoque funcional para el despegue de todo tipo de proyecto.</p>
+      </div>
+      </div>
+    </div>
   </header>
 );
 
