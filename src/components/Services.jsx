929889141b65f1e39bd76d0d49960afc323dddf7
CarlosRuiz <ingecarlosrm@gmail.com>
2020-08-31 23:45:01 -0500

    [UPDATE]Services Styles

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,29 @@ import '../assets/styles/components/Services.scss';
 
 const Services = () => (
   <div className='services--container'>
-    <p>Hola mundo</p>
+    <div className='services--container__title'>
+      <h1>SERVICIOS</h1>
+    </div>
+    <div className='services--cards--container'>
+      <div className='cards--item'>
+        <img src='https://firebasestorage.googleapis.com/v0/b/wakkamole-61f7d.appspot.com/o/gdl-assets%2Fservices%2FProduccion%20Audiovisual%403x.png?alt=media&token=b196e25b-f4cc-49ff-9b56-6af6deab0e4c' alt='' />
+      </div>
+      <div className='cards--item'>
+        <img src='https://firebasestorage.googleapis.com/v0/b/wakkamole-61f7d.appspot.com/o/gdl-assets%2Fservices%2FProduccion%20Audiovisual%403x.png?alt=media&token=b196e25b-f4cc-49ff-9b56-6af6deab0e4c' alt='' />
+      </div>
+      <div className='cards--item'>
+        <img src='https://firebasestorage.googleapis.com/v0/b/wakkamole-61f7d.appspot.com/o/gdl-assets%2Fservices%2FProduccion%20Audiovisual%403x.png?alt=media&token=b196e25b-f4cc-49ff-9b56-6af6deab0e4c' alt='' />
+      </div>
+      <div className='cards--item'>
+        <img src='https://firebasestorage.googleapis.com/v0/b/wakkamole-61f7d.appspot.com/o/gdl-assets%2Fservices%2FProduccion%20Audiovisual%403x.png?alt=media&token=b196e25b-f4cc-49ff-9b56-6af6deab0e4c' alt='' />
+      </div>
+      <div className='cards--item'>
+        <img src='https://firebasestorage.googleapis.com/v0/b/wakkamole-61f7d.appspot.com/o/gdl-assets%2Fservices%2FProduccion%20Audiovisual%403x.png?alt=media&token=b196e25b-f4cc-49ff-9b56-6af6deab0e4c' alt='' />
+      </div>
+      <div className='cards--item'>
+        <img src='https://firebasestorage.googleapis.com/v0/b/wakkamole-61f7d.appspot.com/o/gdl-assets%2Fservices%2FProduccion%20Audiovisual%403x.png?alt=media&token=b196e25b-f4cc-49ff-9b56-6af6deab0e4c' alt='' />
+      </div>
+    </div>
   </div>
 );
 
