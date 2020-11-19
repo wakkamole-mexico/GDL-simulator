6bdb0634b87e2602f29204ebd3bd1316b624fe1e
CarlosRuiz <ingecarlosrm@gmail.com>
2020-11-19 12:41:13 -0600

    [ADD] Clients container styles

---
+++ src/components/Clients.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/styles/components/Clients.scss';
+
+const Clients = () => (
+  <div className='clients'>
+    <div className='client__img'>
+      <img className='client__img--item' src='https://firebasestorage.googleapis.com/v0/b/gdl-9ecdf.appspot.com/o/assets%2FClients%403x.png?alt=media&token=f8fcb1f7-4082-4a0c-9c96-c210562a7456' alt='' />
+    </div>
+    <p>hola mundo</p>
+  </div>
+);
+
+export default Clients;
